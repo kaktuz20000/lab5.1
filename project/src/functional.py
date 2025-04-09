10d403a130661ac9d1eeacbf5a5fce7f625bf88b
kaktuz20000 <149298757+kaktuz20000@users.noreply.github.com>
2025-04-09 13:37:06 +0300

    Удалены папки project и project copy

--- project/src/functional.py
+++
@@ -1,2 +0,0 @@
-def calculate(a: int, b: int) -> int:
-    return a + b
--- project/tests/app_test.py
+++
@@ -1,6 +0,0 @@
-from src.functional import calculate
-
-
-def test_calculate() -> None:
-    assert calculate(2, 3) == 5
-    assert calculate(-1, 1) == 0
